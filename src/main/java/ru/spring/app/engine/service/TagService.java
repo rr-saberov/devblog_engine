ea6aa49e76271f9b3ae8ca3c2250b432e6cb72ab
Ruslan Saberov <60810750+rr-saberov@users.noreply.github.com>
2021-10-09 15:39:53 +0300

    fix GET method for tags

--- src/main/java/ru/spring/app/engine/service/TagService.java
+++ src/main/java/ru/spring/app/engine/service/TagService.java
@@ -29,8 +29,13 @@ public class TagService {
 
     private List<SingleTagResponse> getSingleTagsResponses(String query) {
         List<SingleTagResponse> singleTagResponses = new ArrayList<>();
-        List<TagWithCount> tagWithCounts = tagRepository.getTagsWithCount().stream().filter(tag ->
-                        tag.getTag().contains(query)).collect(Collectors.toList());
+        List<TagWithCount> tagWithCounts;
+        if (query != null) {
+            tagWithCounts = tagRepository.getTagsWithCount().stream().filter(tag ->
+                    tag.getTag().contains(query)).collect(Collectors.toList());
+        } else {
+            tagWithCounts = tagRepository.getTagsWithCount();
+        }
         tagWithCounts.forEach(tag -> singleTagResponses.add(new SingleTagResponse(tag.getTag(),
                 getTagWeight(tag.getTag()))));
         return singleTagResponses;
@@ -39,8 +44,8 @@ public class TagService {
     private Double getTagWeight(String tagName) {
         Tag mostPopularTag = tagRepository.getTagsOrderByPopularity().get(0);
         long postsCount = tagRepository.getPostsCount();
-        double coefficient = 1 /
-                ((double) tagRepository.getPostsCountWithTag(mostPopularTag.getName()) / (double) postsCount);
+        double coefficient =
+                1 / ((double) tagRepository.getPostsCountWithTag(mostPopularTag.getName()) / (double) postsCount);
         return ((double) tagRepository.getPostsCountWithTag(tagName) / postsCount * coefficient);
     }
 }
