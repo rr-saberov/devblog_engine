f6dfd836fe487d79279274f0cd1aed55cf48a988
Ruslan Saberov <60810750+rr-saberov@users.noreply.github.com>
2021-10-16 23:46:41 +0300

    added tests

---
+++ src/test/java/ru/spring/app/engine/service/PostServiceTest.java
@@ -0,0 +1,73 @@
+package ru.spring.app.engine.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getPosts() {
+    }
+
+    @Test
+    void getPostsForModeration() {
+    }
+
+    @Test
+    void getUserPosts() {
+    }
+
+    @Test
+    void getPostsByUserRequest() {
+    }
+
+    @Test
+    void getPostsOnDay() {
+    }
+
+    @Test
+    void getPostsByTag() {
+    }
+
+    @Test
+    void getPostsCountInTheYear() {
+    }
+
+    @Test
+    void getPostById() {
+    }
+
+    @Test
+    void addNewPost() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void getStatistics() {
+    }
+
+    @Test
+    void getUserStatistics() {
+    }
+
+    @Test
+    void addLike() {
+    }
+
+    @Test
+    void addDislike() {
+    }
+
+    @Test
+    void moderatePost() {
+    }
+}
