76768e4a7b68c9e5d8a0c168c8a645b72ca9634b
Ruslan Saberov <60810750+rr-saberov@users.noreply.github.com>
2021-11-29 19:59:57 +0300

    fix sql queries

--- src/main/java/ru/spring/app/engine/entity/PostVotes.java
+++ src/main/java/ru/spring/app/engine/entity/PostVotes.java
@@ -1,6 +1,7 @@
 package ru.spring.app.engine.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,11 +9,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @Entity
 @Table(name = "post_votes")
+@NoArgsConstructor
 public class PostVotes {
 
     @Id
@@ -27,9 +29,15 @@ public class PostVotes {
     private long postId;
 
     @Column(nullable = false)
-    private Date time;
+    private LocalDate time;
 
     @Column(nullable = false)
     private int value;
 
+    public PostVotes(long userId, long postId, LocalDate time, int value) {
+        this.userId = userId;
+        this.postId = postId;
+        this.time = time;
+        this.value = value;
+    }
 }
